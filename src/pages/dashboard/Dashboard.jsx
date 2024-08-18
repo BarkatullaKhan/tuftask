a13c9598532f7989151db50ced8bb78668945f5c
Barkatulla Khan <barkatullakhan71@gmail.com>
2024-08-18 14:35:57 +0530

    added babel

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -16,20 +16,6 @@ import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
 import useBanner from "../../fetcher";
 
-// const fetcher = (...args) => fetch(...args).then((res) => res.json());
-// const useBanner = () => {
-//   // const response = await fetch(`http://localhost:8080/bannerData`);
-//   const { data, error, isLoading } = useSWR(
-//     process.env.REACT_APP_API_BANNER_DATA,
-//     fetcher
-//   );
-
-//   return {
-//     data: data,
-//     isLoading,
-//     isError: error,
-//   };
-// };
 function Dashboard() {
   // const [bannerData, setBannerData] = useState([]);
   const { data, isLoading, isError } = useBanner();
